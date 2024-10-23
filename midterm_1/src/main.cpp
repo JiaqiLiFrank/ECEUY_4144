f3b4631ba6cfadf611d3a2fd5b49a0a9ca5b3128
JiaqiLiFrank <jl13581@nyu.edu>
2024-10-23 15:27:00 -0400

    Comments updated.

--- midterm_1/src/main.cpp
+++ midterm_1/src/main.cpp
@@ -1,32 +1,15 @@
 #include <Arduino.h>
 
-
-/*
-  Bay Lock Valve - PD0
-  Ocean Lock Valve - PD1
-  Bay Lock - PD2
-  Ocean Lock - PD3
-  BL Status - PD4
-  OL Status - PD5
-  Transition - PD6
-
-  Bay Level - PC1, ADC0
-  Transition Level - PC2, ADC1
-  Ocean Level - PC3, ADC2
-*/
-
 void setup() {
   // Set up GPIO
-  // Set up DDR
-  DDRD |= (1<<0);
-  DDRD |= (1<<1);
-  DDRD |= (1<<2);
-  DDRD |= (1<<3);
-  DDRD &= ~(1<<4);
-  DDRD &= ~(1<<5);
-  DDRD &= ~(1<<6);
+  DDRD |= (1<<0); //  Bay Lock Valve    - PD0 - Output
+  DDRD |= (1<<1); //  Ocean Lock Valve  - PD1 - Output
+  DDRD |= (1<<2); //  Bay Lock          - PD2 - Output
+  DDRD |= (1<<3); //  Ocean Lock        - PD3 - Output
+  DDRD &= ~(1<<4); // Bay Lock Status   - PD4 - Input
+  DDRD &= ~(1<<5); // Ocean Lock Status - PD5 - Input
+  DDRD &= ~(1<<6); // Transition Status - PD6 - Input
 
-  // Set up PORT
   PORTD &= ~(1<<PORTD0); // Bay Lock Valve
   PORTD &= ~(1<<PORTD1); // Ocean Lock Valve
   PORTD &= ~(1<<PORTD2); // Bay Lock
@@ -34,8 +17,8 @@ void setup() {
 
   // Set up the ADC
   ADMUX = (1<<REFS0); // Setting the Vcc as the reference
-  ADCSRA = (1<<ADEN) | (1<<ADATE) | (1<<ADPS1) | (1<<ADPS0); // ADC Enable; Auto Trigger Enable; Prescaler 8. 
-  // ADCSRB at Free Running Mode.
+  ADCSRA = (1<<ADEN) | (1<<ADATE) | (1<<ADPS1) | (1<<ADPS0); // ADC Enable; Auto Trigger Enable; Prescaler 8
+  // ADCSRB at Free Running Mode
 }
 
 /*
@@ -46,19 +29,21 @@ bool checkBayTransition(){
   while(true){
     // Set up ADC0 - Bay Level
     DIDR0 = (1<<ADC0D);
-    uint16_t bay_lv = ADCW;
+    uint16_t bay_lv = ADCW; // Bay water level
 
     // Set up ADC1 - Transition Level
     ADMUX |= (1<<MUX0);
     DIDR0 = (1<<ADC1D);
-    uint16_t transition_lv = ADCW;
+    uint16_t transition_lv = ADCW; // Transition waterway water lavel
 
+    // Check if bay water level equals to transition waterway level. If same, return true; else, continue the loop to check. 
     if (bay_lv == transition_lv){
       return true;
     }else{
       continue;
     }
   }
+
   return false;
 }
 
@@ -71,29 +56,30 @@ bool checkTransitionOcean(){
   // Set up ADC1 - Transition Level
   ADMUX |= (1<<MUX0);
   DIDR0 = (1<<ADC1D);
-  uint16_t transition_lv = ADCW;
+  uint16_t transition_lv = ADCW; // Transition waterway water lavel
 
   // Set up ADC2 - Ocean Level
   ADMUX |= (1<<MUX1);
   DIDR0 = (1<<ADC2D);
-  uint16_t ocean_lv = ADCW;
+  uint16_t ocean_lv = ADCW; // Ocean water lavel
 
+  // Check if ocean water level equals to transition waterway level. If same, return true; else, continue the loop to check. 
   if (ocean_lv == transition_lv){
       return true;
     }else{
       continue;
     }
   }
+
   return false;
 }
-
 
 /*
   Continusly checking if the transition waterway is occupied or not.
   OUTPUT: 1 - occupied; 0 - not occupied. 
 */
 bool isTransitionOccupied(){
-  // If the transition is occupied, jump out of the loop and close the lock
+  // If the transition is occupied, return true; else, continue the loop to check. 
   while(true){
     if(PIND & (1<<PIND6)){
       return true;
@@ -104,38 +90,37 @@ bool isTransitionOccupied(){
   return false;
 }
 
+
+/*
+  Moving water from the Bay area, through the transition waterway, and finally reach the ocean area. 
+*/
 uint8_t beginTransitBayToOcean(){
   // Either lock is open or a boat is already in transition
   if ((PIND & (1<<PIND4)) || (PIND & (1<<PIND5)) || (PIND & (1<<PIND6))) {
     return 0; 
   }
 
-  // Open the Bay Lock Valve
-  PORTD |= (1<<PORTD0);
+  PORTD |= (1<<PORTD0); // Open the bay lock valve
 
   if(checkBayTransition){
-    // Open the bay lock if bay level = transition level
-    PORTD |= (1<<PORTD2);
+    PORTD |= (1<<PORTD2); // Open the Bay lock
   }
 
   if(isTransitionOccupied){
-    // Close the bay lock
-    PORTD &= ~(1<<PORTD2);
+    PORTD &= ~(1<<PORTD2); // Close the bay lock
   }
 
   // Check if the bay lock is closed. If closed, open the ocean valve
   if(!(PIND & (1<<PIND2))){
-    // Open Ocean Valve
-    PORTD |= (1<<PORTD1);
+    PORTD |= (1<<PORTD1); // Open ocean valve
   }
 
   if(checkTransitionOcean){
-    // Open the ocean lock if ocean level = transition level
-    PORTD |= (1<<PORTD3);
+    PORTD |= (1<<PORTD3); // Open the ocean lock
   }
 
   if(!isTransitionOccupied){
-    PORTD &= ~(1<<PORTD3);
+    PORTD &= ~(1<<PORTD3); // Close the ocean lock
   }
 
   return 1;
@@ -148,29 +133,27 @@ uint8_t beginTransitOceanToBay(){
     return 0; 
   }
 
-  // Open the Ocean Lock Valve
-  PORTD |= (1<<PORTD1);
+  PORTD |= (1<<PORTD1); // Open the Ocean Lock valve
 
   if(checkTransitionOcean){
-    PORTD |= (1<<PORTD3);
+    PORTD |= (1<<PORTD3); // Open the ocean lock
   }
 
   if(isTransitionOccupied){
-    PORTD &= ~(1<<PORTD3);
+    PORTD &= ~(1<<PORTD3); // Close the ocean lock
   }
 
   // Check if the ocean lock is closed. If closed, open the bay valve
   if(!(PIND & (1<<PIND5))){
-    // Open Bay Valve
-    PORTD |= (1<<PORTD0);
+    PORTD |= (1<<PORTD0); // Open Bay valve
   }
 
   if(checkBayTransition){
-    PORTD |= (1<<PORTD2);
+    PORTD |= (1<<PORTD2); // Open Bay lock
   }
 
   if(!isTransitionOccupied){
-    PORTD &= ~(1<<PORTD2);
+    PORTD &= ~(1<<PORTD2); // Close Bay lock
   }
 
   return 1;
