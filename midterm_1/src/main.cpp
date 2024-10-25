05303414dd2ae95ebe1cb2ecce9ee7873bc3ae28
JiaqiLiFrank <jl13581@nyu.edu>
2024-10-24 20:09:12 -0400

    updated.

--- midterm_1/src/main.cpp
+++ midterm_1/src/main.cpp
@@ -2,165 +2,442 @@
 
 void setup() {
   // Set up GPIO
-  DDRD |= (1<<0); //  Bay Lock Valve    - PD0 - Output
-  DDRD |= (1<<1); //  Ocean Lock Valve  - PD1 - Output
-  DDRD |= (1<<2); //  Bay Lock          - PD2 - Output
-  DDRD |= (1<<3); //  Ocean Lock        - PD3 - Output
-  DDRD &= ~(1<<4); // Bay Lock Status   - PD4 - Input
-  DDRD &= ~(1<<5); // Ocean Lock Status - PD5 - Input
-  DDRD &= ~(1<<6); // Transition Status - PD6 - Input
+  DDRD |= (1<<PD0); //  Bay Lock Valve    - PD0 - Output
+  DDRD |= (1<<PD1); //  Ocean Lock Valve  - PD1 - Output
+  DDRD |= (1<<PD2); //  Bay Lock          - PD2 - Output
+  DDRD |= (1<<PD3); //  Ocean Lock        - PD3 - Output
+  DDRD &= ~(1<<PD4); // Bay Lock Status   - PD4 - Input
+  DDRD &= ~(1<<PD5); // Ocean Lock Status - PD5 - Input
+  DDRD &= ~(1<<PD6); // Transition Status - PD6 - Input
 
+  // Make sure all locks and valves are closed at the beginning
   PORTD &= ~(1<<PORTD0); // Bay Lock Valve
   PORTD &= ~(1<<PORTD1); // Ocean Lock Valve
   PORTD &= ~(1<<PORTD2); // Bay Lock
   PORTD &= ~(1<<PORTD3); // Ocean Lock
 
   // Set up the ADC
-  ADMUX = (1<<REFS0); // Setting the Vcc as the reference
-  ADCSRA = (1<<ADEN) | (1<<ADATE) | (1<<ADPS1) | (1<<ADPS0); // ADC Enable; Auto Trigger Enable; Prescaler 8
+  ADCSRA = (1<<ADEN) | (1<<ADATE) | (1<<ADPS2) | (1<<ADPS1) | (1<<ADPS0);// ADC Enable; Auto Trigger Enable; Prescaler 128
   // ADCSRB at Free Running Mode, thus 0000
 }
 
 /*
-  Continusly checking if the levels of the bay and the transition waterway are the same.
+  Check if the levels of the bay and the transition waterway are the same.
   OUTPUT: 1 - same; 0 - not same.
 */
-bool checkBayTransition(){
+bool checkBayTransitionLevel(){
+  const uint16_t difference = 5; // Reasonable differences between water levels to consider as the same. 
+  const uint16_t timeout = 10000; // 10s to Timeout
+  uint32_t start = millis();
+
   while(true){
     // Set up ADC0 - Bay Level
-    DIDR0 = (1<<ADC0D);
-    uint16_t bay_lv = ADCW; // Bay water level
+    uint16_t bay_lv = readADC(0); // Bay water level
 
-    // Set up ADC1 - Transition Level
-    ADMUX |= (1<<MUX0);
-    DIDR0 = (1<<ADC1D);
-    uint16_t transition_lv = ADCW; // Transition waterway water lavel
+    // Set up ADC1 - Transition Level.
+    uint16_t transition_lv = readADC(1); // Transition waterway water lavel
 
     // Check if bay water level equals to transition waterway level. If same, return true; else, continue the loop to check. 
-    if (bay_lv == transition_lv){
+    if(abs((bay_lv - transition_lv)) <= difference){
       return true;
-    }else{
-      continue;
     }
-  }
 
-  return false;
+    // Timeout Checking
+    if((millis() - start) >= timeout){
+      return false;
+    }
+
+    delay(100);
+  }
 }
 
 /*
-  Continusly checking if the levels of the the transition waterway and ocean are the same.
+  Check if the levels of the the transition waterway and ocean are the same.
   OUTPUT: 1 - same; 0 - not same.
 */
-bool checkTransitionOcean(){
-  while(true) {
-  // Set up ADC1 - Transition Level
-  ADMUX |= (1<<MUX0);
-  DIDR0 = (1<<ADC1D);
-  uint16_t transition_lv = ADCW; // Transition waterway water lavel
+bool checkTransitionOceanLevel(){
+  const uint16_t difference = 5; // Reasonable differences between water levels to consider as the same. 
+  const uint16_t timeout = 10000; // 10s to Timeout
+  uint16_t start = millis();
 
-  // Set up ADC2 - Ocean Level
-  ADMUX |= (1<<MUX1);
-  DIDR0 = (1<<ADC2D);
-  uint16_t ocean_lv = ADCW; // Ocean water lavel
+  while(true){
+    // Set up ADC1 - Transition Level
+    uint16_t transition_lv = readADC(1); // Transition waterway water lavel
 
-  // Check if ocean water level equals to transition waterway level. If same, return true; else, continue the loop to check. 
-  if (ocean_lv == transition_lv){
-      return true;
-    }else{
-      continue;
+    // Set up ADC2 - Ocean Level
+    uint16_t ocean_lv = readADC(2); // Ocean water lavel
+
+    // Check if ocean water level equals to transition waterway level. If same, return true; else, continue the loop to check. 
+    if (abs((ocean_lv - transition_lv)) <= difference){
+        return true;
+      }
+
+    // Timeout Checking
+    if((millis() - start) >= timeout){
+      return false;
     }
-  }
 
-  return false;
+    delay(100);
+  }
 }
 
 /*
-  Continusly checking if the transition waterway is occupied or not.
+  Checking if the transition waterway is occupied or not.
   OUTPUT: 1 - occupied; 0 - not occupied. 
 */
-bool isTransitionOccupied(){
+bool checkIsTransitionOccupied(){
   // If the transition is occupied, return true; else, continue the loop to check. 
-  while(true){
     if(PIND & (1<<PIND6)){
       return true;
     }else{
-      continue;
+      return false;
     }
+}
+
+/*
+  Switch between ADC channels and read data.
+  OUTPUT: ADCW data from ADC, 16-bit.
+  @param cs chip/channel select
+*/
+uint16_t readADC(uint8_t cs){
+  if(cs == 0){
+    ADMUX = (1<<REFS0);// Set reference and channel
+    DIDR0 = (1<<ADC0D);
+    ADCSRA |= (1 << ADSC); // Start conversion
+    return ADCW; // Return the ADC value
+  }else{
+    ADMUX = (1<<REFS0) | (1<<cs); // Set reference and channel
+    DIDR0 = (1<<cs);
+    ADCSRA |= (1 << ADSC); // Start conversion
+    return ADCW; // Return the ADC value
+  }
+}
+
+/*
+  Open the Bay Lock Valve
+*/
+bool openBayValve(){
+  PORTD |= (1<<PORTD0); // Open the bay lock valve
+  delay(100);
+
+  // Check if the Bay Valve is opened successfully
+  if(PORTD & (1<<PD0)){
+    return true;
   }
   return false;
 }
 
+/*
+  Close the Bay Lock Valve
+*/
+bool closeBayValve(){
+  PORTD &= ~(1<<PORTD0); // Close the bay lock valve
+  delay(100);
+
+  // Check if the Bay Valve is closed successfully
+  if(!(PORTD & (1<<PD0))){
+    return true;
+  }
+  return false;
+}
+
+/*
+  Open the Ocean Lock Valve
+*/
+bool openOceanValve(){
+  PORTD |= (1<<PORTD1); // Open ocean valve
+  delay(100);
+
+  // Check if the Ocean Valve is opened successfully
+  if(PORTD & (1<<PD1)){
+    return true;
+  }
+  return false;
+}
+
+/*
+  Close the Ocean Lock Valve
+*/
+bool closeOceanValve(){
+  PORTD &= ~(1<<PORTD1); // Close ocean valve
+  delay(100);
+
+  // Check if the Ocean Valve is closed successfully
+  if(!(PORTD & (1<<PD1))){
+    return true;
+  }
+  return false;
+}
+
+/*
+  Open the Bay Lock
+*/
+bool openBayLock(){
+  PORTD |= (1<<PORTD2); // Open the Bay lock
+  delay(100);
+
+  // Close the Bay Valve
+  if(closeBayValve() && (PIND & (1<<PIND4))){
+    return true;
+  }
+  return false;
+}
+
+/*
+  Close the Bay Lock
+*/
+bool closeBayLock(){
+  PORTD &= ~(1<<PORTD2); // Close the Bay lock
+  delay(100);
+
+  // Check if the Bay Lock is closed successfully
+  if(!(PIND & (1<<PIND4))){
+    return true;
+  }
+  return false;
+}
+
+/*
+  Open the Ocean Lock
+*/
+bool openOceanLock(){
+  PORTD |= (1<<PORTD3); // Open the ocean lock
+  delay(100);
+
+  // Check if the Ocean Lock is opened successfully
+  if(closeOceanValve() && (PIND & (1<<PIND5))){
+    return true;
+  }
+  return false;
+}
+
+/*
+  Close the Ocean Lock
+*/
+bool closeOceanLock(){
+  PORTD &= ~(1<<PORTD3); // Close the ocean lock
+  delay(100);
+
+  // Check if the Ocean Lock is closed successfully
+  if(!(PIND & (1<<PIND5))){
+    return true;
+  }
+  return false;
+}
 
 /*
   Moving water from the Bay area, through the transition waterway, and finally reach the ocean area. 
 */
 uint8_t beginTransitBayToOcean(){
   // Either lock is open or a boat is already in transition
-  if ((PIND & (1<<PIND4)) || (PIND & (1<<PIND5)) || (PIND & (1<<PIND6))) {
+  if((PIND & (1<<PIND4)) || (PIND & (1<<PIND5)) || (PIND & (1<<PIND6))){
     return 0; 
   }
 
-  PORTD |= (1<<PORTD0); // Open the bay lock valve
+  uint8_t step = 1; // Step counter
 
-  if(checkBayTransition){
-    PORTD |= (1<<PORTD2); // Open the Bay lock
+  /*
+    Steps:  1. open bay valve
+            2. check bay and transition water level
+            2. open bay lock
+            4. check transition occupied
+            5. close bay lock
+            6. open ocean valve
+            7. check transition ocean water level
+            8. open ocean lock
+            9. check transition occupied
+            10. close ocean lock
+  */
+  while(true){
+    switch (step){
+      case 1:
+        if(openBayValve()){
+          step = 2;
+          break;
+        }else{
+          return 2;
+        }
+      case 2:
+        if(checkBayTransitionLevel()){
+          step = 3;
+          break;
+        }else{
+          step = 2;
+          break;
+        }
+      case 3:
+        if(openBayLock()){
+          step = 4;
+          break;
+        }else{
+          return 2;
+        }
+      case 4:
+        if(checkIsTransitionOccupied()){
+          step = 5;
+          break;
+        }else{
+          step = 4;
+          break;
+        }
+      case 5:
+        if(closeBayLock()){
+          step = 6;
+          break;
+        }else{
+          return 2;
+        }
+      case 6:
+        if(openOceanValve()){
+          step = 7;
+          break;
+        }else{
+          return 2;
+        }
+      case 7:
+        if(checkTransitionOceanLevel()){
+          step = 8;
+          break;
+        }else{
+          step = 7;
+          break;
+        }
+      case 8:
+        if(openOceanLock()){
+          step = 9;
+          break;
+        }else{
+          return 2;
+        }
+      case 9:
+        if(!checkIsTransitionOccupied()){
+          step = 10;
+          break;
+        }else{
+          step = 9;
+          break;
+        }
+      case 10:
+        if(closeOceanLock()){
+          return 1;
+        }else{
+          return 2;
+        }
+
+      default:
+        return 2;
+    }
+
+    delay(100);
   }
-
-  if(isTransitionOccupied){
-    PORTD &= ~(1<<PORTD2); // Close the bay lock
-  }
-
-  // Check if the bay lock is closed. If closed, open the ocean valve
-  if(!(PIND & (1<<PIND2))){
-    PORTD |= (1<<PORTD1); // Open ocean valve
-  }
-
-  if(checkTransitionOcean){
-    PORTD |= (1<<PORTD3); // Open the ocean lock
-  }
-
-  if(!isTransitionOccupied){
-    PORTD &= ~(1<<PORTD3); // Close the ocean lock
-  }
-
-  return 1;
-
 }
 
-uint8_t beginTransitOceanToBay(){
+uint8_t beginTransitOceanToBay(){  
   // Either lock is open or a boat is already in transition
   if ((PIND & (1<<PIND4)) || (PIND & (1<<PIND5)) || (PIND & (1<<PIND6))) {
     return 0; 
   }
+  
+  uint8_t step = 1; // Step counter
 
-  PORTD |= (1<<PORTD1); // Open the Ocean Lock valve
+  /*
+    Steps:  1. open ocean valve
+            2. check transition ocean water level
+            2. open ocean lock
+            4. check transition occupied
+            5. close ocean lock
+            6. open bay valve
+            7. check transition bay water level
+            8. open bay lock
+            9. check transition occupied
+            10. close bay lock
+  */
+  while(true){
+    switch (step){
+      case 1:
+        if(openOceanValve()){
+          step = 2;
+          break;
+        }else{
+          return 2;
+        }
+      case 2:
+        if(checkTransitionOceanLevel()){
+          step = 3;
+          break;
+        }else{
+          step = 2;
+          break;
+        }
+      case 3:
+        if(openOceanLock()){
+          step = 4;
+          break;
+        }else{
+          return 2;
+        }
+      case 4:
+        if(checkIsTransitionOccupied()){
+          step = 5;
+          break;
+        }else{
+          step = 4;
+          break;
+        }
+      case 5:
+        if(closeOceanLock()){
+          step = 6;
+          break;
+        }else{
+          return 2;
+        }
+      case 6:
+        if(openBayValve()){
+          step = 7;
+          break;
+        }else{
+          return 2;
+        }
+      case 7:
+        if(checkTransitionOceanLevel()){
+          step = 8;
+          break;
+        }else{
+          step = 7;
+          break;
+        }
+      case 8:
+        if(openBayLock()){
+          step = 9;
+          break;
+        }else{
+          return 2;
+        }
+      case 9:
+        if(!checkIsTransitionOccupied()){
+          step = 10;
+          break;
+        }else{
+          step = 9;
+          break;
+        }
+      case 10:
+        if(closeBayLock()){
+          return 1;
+        }else{
+          return 2;
+        }
 
-  if(checkTransitionOcean){
-    PORTD |= (1<<PORTD3); // Open the ocean lock
+      default:
+        return 2;
+    }
+
+    delay(100);
   }
-
-  if(isTransitionOccupied){
-    PORTD &= ~(1<<PORTD3); // Close the ocean lock
-  }
-
-  // Check if the ocean lock is closed. If closed, open the bay valve
-  if(!(PIND & (1<<PIND5))){
-    PORTD |= (1<<PORTD0); // Open Bay valve
-  }
-
-  if(checkBayTransition){
-    PORTD |= (1<<PORTD2); // Open Bay lock
-  }
-
-  if(!isTransitionOccupied){
-    PORTD &= ~(1<<PORTD2); // Close Bay lock
-  }
-
-  return 1;
-
 }
 
-void loop() {
-  beginTransitBayToOcean();
+void loop(){
+  // Since there is no description on when to call the functions, I directly pasted two fucntions to the loop(). 
+  beginTransitOceanToBay();
   beginTransitBayToOcean();
 }
