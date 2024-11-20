b34ef01b23a4c7e0b57f0dba57e61aa55e5cf5d5
JiaqiLiFrank <jl13581@nyu.edu>
2024-11-20 10:51:54 -0500

    updated.

--- acc_test/src/main.cpp
+++ acc_test/src/main.cpp
@@ -1,18 +1,10 @@
-#include <Adafruit_CircuitPlayground.h>
-
+#include <Arduino.h>
+#include <SPI.h>
 void setup(){
-  CircuitPlayground.begin();
-  Serial.begin(115200);
+
 }
 
 void loop(){
-  float x = CircuitPlayground.motionX();
-  float y = CircuitPlayground.motionY();
-  float z = CircuitPlayground.motionZ();
 
-  float pos = sqrt((x*x+y*y+z*z));
 
-  Serial.print(">pos:");
-  Serial.println(pos);
-  delay(20);
 }
