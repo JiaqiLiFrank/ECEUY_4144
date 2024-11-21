01c97e83fa46bf0261da45e39b43dbca266e4143
JiaqiLiFrank <jl13581@nyu.edu>
2024-11-21 18:00:25 -0500

    updated.

--- hands_on_communication/src/main.cpp
+++ hands_on_communication/src/main.cpp
@@ -5,15 +5,18 @@ void USART_Init(){
     Normal transmission speed, 
     disable the multi-processor communication mode */
   UCSR1A = 0x00;
+
   UCSR1B = 0x00;          // Reset the UCSR1B
   UCSR1B |= (1<<RXCIE1) | // Turn on RX Complete Interrupt
             (1<<UDRIE1) | // Turn on the data register empty interrupt
             (1<<RXEN1);   // Turn on the receiver
   UCSR1B &= ~(1<<TXEN1);  // Turn off the Transmitter
   UCSR1B &= ~(1<<TXCIE1);  // Turn off TX Complete Interrupt
+
   UCSR1C = 0x00;          // Reset the UCSR1C
   UCSR1C |= (1<<UCSZ11) | // Character Size to 8-bit
             (1<<UCSZ10);
+            
   UBRR1 = 51;             // UBRR1 = (fosc / (16*Baud Rate)) - 1
                           //       = (8MHz/(16*9600))-1 = 51.08
 }
