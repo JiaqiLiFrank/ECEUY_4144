ca8acd80145c946082b328f2c6e5c4a26d274219
JiaqiLiFrank <jl13581@nyu.edu>
2024-11-26 18:15:32 -0500

    updated.

--- lab3/src/main.cpp
+++ lab3/src/main.cpp
@@ -6,9 +6,7 @@ void setup() {
     UCSR1C = 0x00; // UCSRC set as default.
 
     UCSR1B |= (1<<RXEN1) | // Enable receiver
-              (1<<TXEN1) | // Enable transmitter
-              (1<<RXCIE1)| // Enable RX Interrupt
-              (1<<TXCIE1); // Enable TX Interrupt
+              (1<<TXEN1); // Enable transmitter
     UCSR1C |= (1<<UCSZ10) |
               (1<<UCSZ11); // 8-bit data
     UBRR1 = 51; // UBRR1 = (fosc / (16 * Baud Rate)) - 1
@@ -17,32 +15,17 @@ void setup() {
     DDRF &= ~(1<<PF6); // Right button PF6 as input
     DDRC |= (1<<PC7); // Set PC7 as output
     PORTC &= ~(1<<PC7); // Set LED to off
+
 }
 
-ISR(USART1_RX_vect){
-    uint16_t receivedData = UDR1; // Read received data
-    UDR1 = receivedData; // Send received data
-}
 
 void loop() {
     // Team A
     if(PINF & (1<<PF6)){
+        PORTC |= (1<<PC7); // Set LED to on
         UDR1 = 0x31; // Send 1
     } else {
+        PORTC &= ~(1<<PC7); // Set LED to off
         UDR1 = 0x30; // Send 0
     }
-    // if(UDR1 == 0x31){
-    //     PORTC |= (1<<PC7); // Set LED to on
-    // } else {
-    //     PORTC &= ~(1<<PC7); // Set LED to off
-    // }
-
-    // // Team B
-    // if(UDR1 == 0x31){
-    //     PORTC |= (1<<PC7); // Set LED to on
-    //     UDR1 = 0x31; // Send 1
-    // } else {
-    //     PORTC &= ~(1<<PC7); // Set LED to off
-    //     UDR1 = 0x30; // Send 0
-    // }
 }
