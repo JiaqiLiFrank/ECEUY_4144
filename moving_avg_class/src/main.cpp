67f94d6626d09768cb95f175e8fc40699cfb2bd7
JiaqiLiFrank <jl13581@nyu.edu>
2024-11-27 10:57:06 -0500

    updated.

--- moving_avg_class/src/main.cpp
+++ moving_avg_class/src/main.cpp
@@ -1,18 +1,35 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+// Use the moving average method to clean up the messy signal from ADC. 
+// Simulating an ECG, which received electro signals from our body. 
+// INFO: Digital D9, PB5, ADC12
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  pinMode(A9, INPUT_PULLUP); // If no signal, resistor is connected to the voltage input
+  Serial.begin(115200);
+}
+
+#define BUFFER_SIZE 7               // Set up the moving average contant as 7
+uint16_t buffer[BUFFER_SIZE] = {0}; // Buffer to store the values
+uint16_t currentAvg;                // Current average value
+
+/*
+  * Moving average method
+  * 1. Shift the buffer to the left
+  * 2. Add the new value to the buffer
+  * 3. Calculate the average value
+*/
+uint16_t moving_average(){
+  for(int i = 0; i < BUFFER_SIZE - 1; i++){
+    buffer[i] = buffer[i + 1]; // Shift the buffer to the left
+    currentAvg += buffer[i];   // Sum up the values
+  }
+  buffer[BUFFER_SIZE - 1] = analogRead(A9); // Add the new value to the buffer
+  currentAvg += buffer[BUFFER_SIZE - 1];    // Sum up the values
+  currentAvg /= BUFFER_SIZE;                // Calculate the average value
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  Serial.println(analogRead(A9));
+  delay(500);
 }
